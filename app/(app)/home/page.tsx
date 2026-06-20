588413dbd3f6273f708f9e2840c6a31f714fa427
uridelta1 <uridelta1@gmail.com>
2026-06-20 18:42:07 +0530

    fix prisma client generation dd

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -1,4 +1,4 @@
-// "use client"
+"use client"
 // import React, {useState, useEffect, useCallback} from 'react'
 // import axios from 'axios'
 // import VideoCard from '../../../components/videoCard'
